b4a17b069d51f3dd6bdcb79f03fb8f3fdaabc5e5
Alazar-dev <mealazartadesse@gmail.com>
2021-11-03 16:25:45 +0300

    betty code

--- char.c
+++ char.c
@@ -3,13 +3,12 @@
 /**
  * printchar - outputs characters
  *
- * @list_args - list of arguments
+ * list_args - list of arguments
  *
  * Return: int
  */
 int printchar(va_list list_args)
 {
-  _putchar(va_arg(list_args, int));
-  return (1);
+_putchar(va_arg(list_args, int));
+return (1);
 }
- 
